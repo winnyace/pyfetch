1b1bb1f9165c72d80be060f30f8a14704901aa8a
winnyace <winnyace@proton.me>
2024-05-10 07:35:14 +0300

    More consistent variable names

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ def get_distro(r):
                 temp = line[line.find("\""):]
                 temp = temp.replace('"', '')
                 temp = temp.rstrip()
-    unformat = "OS:\t\t" + temp
+    unformat = "OS:\t" + temp
     r.append(unformat)
 
 
 def get_kernel(r):
-    command = subprocess.run(["uname", "-srm"], stdout=subprocess.PIPE).stdout.decode("utf-8").rstrip()
-    final = "Kernel:\t" + command.strip("Linux ")
+    command = subprocess.run(["uname", "-rm"], stdout=subprocess.PIPE).stdout.decode("utf-8").rstrip()
+    final = "Kernel:\t" + command
     r.append(final)
 
 
@@ -36,11 +36,11 @@ def get_uptime(r):
 
 def get_memory(r):
     with open("/proc/meminfo", "r") as file:
-        memtotal = file.readline().strip("MemTotal:\t").lstrip().rstrip().strip("kB")
+        mem_total = file.readline().strip("MemTotal:\t").lstrip().rstrip().strip("kB")
         file.readline()
-        memavail = file.readline().strip("MemAvailable:\t").lstrip().rstrip().strip("kB")
-    memused = int(memtotal)//1024 - int(memavail)//1024
-    final = "Memory:\t" + str(memused) + "M / " + str(int(memtotal)//1024) + "M"
+        mem_avail = file.readline().strip("MemAvailable:\t").lstrip().rstrip().strip("kB")
+    mem_used = int(mem_total)//1024 - int(mem_avail)//1024
+    final = "Memory:\t" + str(mem_used) + "M / " + str(int(mem_total)//1024) + "M"
     r.append(final)
 
 
@@ -48,13 +48,13 @@ def get_cpu(r):
     with open("/proc/cpuinfo", "r") as file:
         for i in range(4):
             file.readline()
-        unformattedcpu = re.search(": ", file.readline())
-        cpu = unformattedcpu.string.strip("model name:\t").rstrip()
+        unformat_cpu = re.search(": ", file.readline())
+        cpu = unformat_cpu.string.strip("model name:\t").rstrip()
     final = "CPU:\t" + cpu
     r.append(final)
 
 
-def get_moboinfo(r):
+def get_mobo_info(r):
     with open("/sys/devices/virtual/dmi/id/product_name") as file:
         name = file.read().rstrip()
     with open("/sys/devices/virtual/dmi/id/product_version") as file:
@@ -68,7 +68,7 @@ if __name__ == "__main__":
 
     get_user(info)
     get_distro(info)
-    get_moboinfo(info)
+    get_mobo_info(info)
     get_cpu(info)
     get_memory(info)
     get_kernel(info)
